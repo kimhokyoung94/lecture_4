04892ba59bf3ee0cacb462ae5419cd6820d96f76
HoKoung Kim <ghrud24020@naver,com>
2020-07-17 17:44:34 +0900

    20200717

--- func_add.py
+++ func_add.py
@@ -12,6 +12,6 @@ def add_plus(x,y,z):
     result_1 = x+y
     result_2 = lambda y : y**3
 
-    return int(result_1 , result_2)
+    return result_1 , result_2
 
 print(add_plus(10,30,20))
--- vector2d_error.py
+++ vector2d_error.py
@@ -1,3 +1,4 @@
+#TypeError: unsupported operand type(s) for +: 'Vector2D' and 'Vector2D'
 class Vector2D():
     def __init__(self,x,y):
         self.x = x
@@ -7,4 +8,4 @@ class Vector2D():
 
 v1 = Vector2D(1,2)
 v2 = Vector2D(3,4)
-v3 = v1+v2
+v3 = v1+v2 #생성자만 만든다고 해서 그 값을 더할 수 있는 것이 아니다.
